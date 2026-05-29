cdf465af6263ef50b5a97049241c6c11425d1be9
Abisaac1809 <abisaaccarmona@gmail.com>
2026-05-29 13:52:19 -0400

    feat: rabbitmq producer in space service to send messages when a space is created or updated

--- services/SpaceService/models/espacio.go
+++ services/SpaceService/models/espacio.go
@@ -5,12 +5,13 @@ import (
 )
 
 type Espacio struct {
-	ID            uint      `gorm:"primaryKey;autoIncrement" json:"id"`
-	Nombre        string    `gorm:"type:varchar(100);not null" json:"nombre"`
-	Descripcion   string    `gorm:"type:text" json:"descripcion"`
-	Capacidad     int       `gorm:"type:integer;not null" json:"capacidad"`
-	PrecioPorHora float64   `gorm:"type:decimal(10,2);not null" json:"precio_por_hora"`
-	Estado        string    `gorm:"type:varchar(20);default:'disponible'" json:"estado"`
-	FotoURL       string    `gorm:"type:varchar(255);column:foto_url" json:"foto_url"`
-	CreadoEn      time.Time `gorm:"type:timestamp;default:CURRENT_TIMESTAMP;column:creado_en" json:"creado_en"`
+	ID                   uint      `gorm:"primaryKey;autoIncrement" json:"id"`
+	Nombre               string    `gorm:"type:varchar(100);not null" json:"nombre"`
+	Descripcion          string    `gorm:"type:text" json:"descripcion"`
+	Capacidad            int       `gorm:"type:integer;not null" json:"capacidad"`
+	PrecioPorHora        float64   `gorm:"type:decimal(10,2);not null" json:"precio_por_hora"`
+	Estado               string    `gorm:"type:varchar(20);default:'disponible'" json:"estado"`
+	NecesitaVerificacion bool      `gorm:"type:boolean;default:false;column:necesita_verificacion" json:"necesita_verificacion"`
+	FotoURL              string    `gorm:"type:varchar(255);column:foto_url" json:"foto_url"`
+	CreadoEn             time.Time `gorm:"type:timestamp;default:CURRENT_TIMESTAMP;column:creado_en" json:"creado_en"`
 }
